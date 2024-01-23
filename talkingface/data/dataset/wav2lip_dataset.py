930fa26947ae3b1bcbc6d2ae0fb9a26c905889e2
Kline-song <527811545@qq.com>
2024-01-23 23:17:12 +0800

    最新修改

--- talkingface/data/dataset/wav2lip_dataset.py
+++ talkingface/data/dataset/wav2lip_dataset.py
@@ -1,13 +1,8 @@
 from os.path import dirname, join, basename, isfile
 from tqdm import tqdm
 from talkingface.data.dataprocess.wav2lip_process import Wav2LipAudio
-import python_speech_features
 
 import torch
-from torch import nn
-from torch import optim
-import torch.backends.cudnn as cudnn
-from torch.utils import data as data_utils
 import numpy as np
 import librosa
 from talkingface.data.dataset.dataset import Dataset
@@ -16,16 +11,17 @@ from glob import glob
 
 import os, random, cv2, argparse
 
+
 class Wav2LipDataset(Dataset):
     def __init__(self, config, datasplit):
         super().__init__(config, datasplit)
         self.all_videos = self.get_image_list(self.config['preprocessed_root'], datasplit)
         self.audio_processor = Wav2LipAudio(self.config)
 
-    
     def get_image_list(self, data_root, split_path):
         filelist = []
-
+        print("split path:")
+        print(split_path)
         with open(split_path) as f:
             for line in f:
                 line = line.strip()
@@ -34,7 +30,6 @@ class Wav2LipDataset(Dataset):
 
         return filelist
 
-    
     def get_frame_id(self, frame):
         return int(basename(frame).split('.')[0])
 
@@ -70,17 +65,17 @@ class Wav2LipDataset(Dataset):
         if type(start_frame) == int:
             start_frame_num = start_frame
         else:
-            start_frame_num = self.get_frame_id(start_frame) # 0-indexing ---> 1-indexing
+            start_frame_num = self.get_frame_id(start_frame)  # 0-indexing ---> 1-indexing
         start_idx = int(80. * (start_frame_num / float(self.config['fps'])))
-        
+
         end_idx = start_idx + self.config['syncnet_mel_step_size']
 
-        return spec[start_idx : end_idx, :]
+        return spec[start_idx: end_idx, :]
 
     def get_segmented_mels(self, spec, start_frame):
         mels = []
         assert self.config['syncnet_T'] == 5
-        start_frame_num = self.get_frame_id(start_frame) + 1 # 0-indexing ---> 1-indexing
+        start_frame_num = self.get_frame_id(start_frame) + 1  # 0-indexing ---> 1-indexing
         if start_frame_num - 2 < 0: return None
         for i in range(start_frame_num, start_frame_num + self.config['syncnet_T']):
             m = self.crop_audio_window(spec, i - 2)
@@ -109,7 +104,7 @@ class Wav2LipDataset(Dataset):
             img_names = list(glob(join(vidname, '*.jpg')))
             if len(img_names) <= 3 * self.config['syncnet_T']:
                 continue
-            
+
             img_name = random.choice(img_names)
             wrong_img_name = random.choice(img_names)
             while wrong_img_name == img_name:
@@ -135,7 +130,7 @@ class Wav2LipDataset(Dataset):
             except Exception as e:
                 continue
 
-            mel = self.crop_audio_window(orig_mel.copy(), img_name) 
+            mel = self.crop_audio_window(orig_mel.copy(), img_name)
 
             if (mel.shape[0] != self.config['syncnet_mel_step_size']):
                 continue
@@ -145,7 +140,7 @@ class Wav2LipDataset(Dataset):
 
             window = self.prepare_window(window)
             y = window.copy()
-            window[:, :, window.shape[2]//2:] = 0.
+            window[:, :, window.shape[2] // 2:] = 0.
 
             wrong_window = self.prepare_window(wrong_window)
             x = np.concatenate([window, wrong_window], axis=0)
@@ -155,4 +150,4 @@ class Wav2LipDataset(Dataset):
             # breakpoint()
             indiv_mels = torch.FloatTensor(indiv_mels).unsqueeze(1)
             y = torch.FloatTensor(y)
-            return {"input_frames":x, "indiv_mels":indiv_mels, "mels":mel, "gt":y}
+            return {"input_frames": x, "indiv_mels": indiv_mels, "mels": mel, "gt": y}
