740dbecbd7a7cd0db9567615621ca2773e7eb0c7
alicee10 <143008146+alicee10@users.noreply.github.com>
2024-01-30 18:37:42 +0800

    Delete dataset/filelist directory

--- dataset/filelist/mk_filelist.py
+++
@@ -1,8 +0,0 @@
-import glob
-
-file_list = glob.glob("/home/hlj/danzi/talkingface-toolkit-main/dataset/vctk/data/*/*.wav")
-
-with open("filelist.txt", 'w') as f:
-    for item in file_list:
-        wav_name, spk = item.split("/")[-1], item.split("/")[-2] 
-        f.write(wav_name+"\n")
